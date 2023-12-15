777f14108fc5ee0c089e739a0cff242ce0c6ca2e
Vignesh2003-git <cvignesh112@gmail.com>
2023-12-15 23:45:58 +0530

    ultil Function Added

---
+++ util.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad requests
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return None
+
+def extract_text_from_html(html):
+    if html is None:
+        return None
+
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text(separator='\n', strip=True)
+    return text
+
+def get_html_contents_of_urls(url_list):
+    html_contents = []
+    for url in url_list:
+        html = get_html_content(url)
+        text_content = extract_text_from_html(html)
+        html_contents.append(text_content)
+    return html_contents
+
+# Example usage:
+urls = ["https://en.wikipedia.org/", "https://www.javatpoint.com/"]
+result = get_html_contents_of_urls(urls)
+print(result)
