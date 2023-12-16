dee595022e88333b14ecde57265e5769a80605ad
Michael Pio <michaelmathurapioa@gmail.com>
2023-12-16 12:36:36 +0530

    Ai Manager Template Added

---
+++ AiMan.py
@@ -0,0 +1,16 @@
+from util import read_credentials_file
+class AiMan:
+    def __main__(self):
+        self.cred_json = read_credentials_file("Credentials.json")
+
+    def ProcessContentData(self,Content:str):
+        pass
+
+    def Produce_ImageURL(self,Content:str):
+        pass
+
+    def Produce_AudioURL(self,Content:str):
+        pass
+
+    def Produce_YoutubeVideoURL(self,Content:str):
+        pass
